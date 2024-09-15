b606fc855c9afbe36fa402ad1455790d2a048d90
osmrDev <osmrdevloper@gmail.com>
2024-09-14 20:58:23 -0700

    Fixed background scaling issue for square window

--- Modules_Common/makeBg.js
+++ Modules_Common/makeBg.js
@@ -7,8 +7,10 @@ function makeBackground(asset) {
   var bgObj = new PIXI.Sprite(bgImg);
 
 
-  if(GV_app.screen.width > GV_app.screen.height) place(bgObj, 5, 0, 0, GV_app.screen.width / asset[1])
-  else  place(bgObj, 5, 0, 0, GV_app.screen.height / asset[2])
+  if(GV_app.screen.width/4/3 > GV_app.screen.height) place(bgObj, 5, 0, 0, GV_app.screen.width / asset[1])
+  else place(bgObj, 5, 0, 0, GV_app.screen.height / asset[2])
+
+  console.log(GV_app.screen.width)
   //scale to width
   //var scale = GV_app.screen.width / asset[1];
   //bgObj.scale.set(scale, scale)
